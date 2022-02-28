d9769e119481f5e3c22648d9c91b496f2709d9a2
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2022-02-27 22:17:25 -0800

    changes some active record relationships to simplify and streamline modeling

--- foam_backend/app/models/image.rb
+++ foam_backend/app/models/image.rb
@@ -1,4 +1,3 @@
 class Image < ApplicationRecord
     has_many :tags
-    has_many :users, throught: :tags
 end
--- foam_backend/app/models/user.rb
+++ foam_backend/app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
     has_many :tags
-    has_many :images, through: :tags 
     has_secure_password
 end
