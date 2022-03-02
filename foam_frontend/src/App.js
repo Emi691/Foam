3073d30e2fb178a7015cc33272a5c573feab8b3b
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2022-03-02 12:28:02 -0800

    sets up fetch request to get images on front_end

--- foam_frontend/src/App.js
+++ foam_frontend/src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Tag from './components/tags'
 import Container from './containers/imagesContainer'
 
 function App() {
   return (
     <div className="App">
-      <Tag/>
       <Container/>
     </div>
   );
--- foam_frontend/src/containers/imagesContainer.js
+++ foam_frontend/src/containers/imagesContainer.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from '../components/images'
+import Tag from '../components/tags'
 
-function imagesContainer(props) {
+function ImagesContainer(props) {
+    const [state, setState] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:3000/images")
+        .then(res => res.json())
+        .then(image => console.log(image.data))
+    })
+
     return (
-        <div style={{backgroundColor:"blue"}}>
+        <div className='container'>
             container
             <Image/>
+            <Tag/>
         </div>
     );
 }
 
-export default imagesContainer;
+export default ImagesContainer;
